073c826fddc88993d33efba6ca81721e063c7f55
Ivan <naviivan321@gmail.com>
2020-02-29 09:37:20 -0700

    Got the website connected to the API

--- script.js
+++ script.js
@@ -1,53 +1,42 @@
 //==========================================================
 //this is an async request being made to OpenWeather api to request Data from the specified
 function fetch_json (city){
-    var weather;
-    fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=249e80fba61f245e94d010e0f9955e10").then(
+    fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=249e80fba61f245e94d010e0f9955e10&units=metric").then(
     function (response){
         return response.json()
         }).then(function (data){
-            weather = data;
-            document.getElementById("table").firstChild.appendChild(add_row(data));
-    }).then(function(response) {console.log(weather)}).catch(function (err){
-        weather = "City is not in the OpenWeather API";
+            window.addEventListener('load',add_row(data));
+    }).catch(function (err){
+        alert("City is not in the OpenWeather API");
     })
-    return weather;
 }
 //==========================================================
+
+function create_entry(text){
+    //this wil be an entry in a row
+    return document.createElement("TD").appendChild(document.createTextNode(text));
+}
 function clicked(){
     fetch_json(document.getElementById("input-box").value);
     
 }
-function create_entry(text){
-    //this wil be an entry in a row
-    return document.createElement("td").append(document.createTextNode(text));
-}
-/*
-function create_table_header(header_list){
-    //header_list = [City,temp,feels_like,temp_min,temp_max,pressure,humidity,visibility,forecast,description]
-    var node = document.createElement("tr").appendChild(document.createElement("th"));
-    document.getElementById("table").appendChild(node);
-    for (var i = 0;i<header_list.length; i++){
-        //.firstChild.firstChild is the node containing the headers
-        document.getElementById("table").firstChild.firstChild.appendChild(node.appendChild(document.createTextNode(header_list[i])));
-    }
-}*/
 function add_row(weather_json){
-    var root_node = document.createElement("tr");
-    console.log("REEEE1111");
-    root_node.appendChild(create_entry(weather_json.name));
-    console.log("REEEE");
-    root_node.appendChild(create_entry(weather_json.main.temp));
-    root_node.appendChild(create_entry(weather_json.main.feels_like));
-    root_node.appendChild(create_entry(weather_json.main.temp_min));
-    root_node.appendChild(create_entry(weather_json.main.temp_max));
-    root_node.appendChild(create_entry(weather_json.main.pressure));
-    root_node.appendChild(create_entry(weather_json.main.humidity));
-    root_node.appendChild(create_entry(weather_json.visibility));
-    root_node.appendChild(create_entry(weather_json.weather[0].main));
-    root_node.appendChild(create_entry(weather_json.weather[0].description));
-    
-    return root_node;
-    
-    
+   const table = document.getElementById("table-body");
+   var iconurl = "http://openweathermap.org/img/w/" + weather_json.weather[0].icon + ".png";
+   let icon = `<img id="wicon" src="${iconurl}" alt="Weather icon">`;
+   let table_row = 
+    `<tr>`+ 
+        `<td>${weather_json.name}</td>`+
+        `<td>${weather_json.main.temp}</td>`+
+        `<td>${weather_json.main.feels_like}</td>`+
+        `<td>${weather_json.main.temp_min}</td>`+
+        `<td>${weather_json.main.temp_max}</td>`+
+        `<td>${weather_json.main.pressure}</td>`+
+        `<td>${weather_json.main.humidity}%</td>`+
+        `<td>${weather_json.visibility}</td>`+
+        `<td>${icon}</td>`+
+        `<td>${weather_json.weather[0].description}</td>`+
+    `</tr>`;
+
+    table.innerHTML += table_row;
 }
